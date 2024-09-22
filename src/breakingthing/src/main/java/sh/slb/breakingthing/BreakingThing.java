c170d24429cc8a7d699fc20a39c8840f87211c66
slbsh <crafter.antek@gmail.com>
2024-09-22 19:33:15 +0200

    feat: add semaphore separator blocks

--- src/breakingthing/src/main/java/sh/slb/breakingthing/BreakingThing.java
+++
@@ -1,8 +0,0 @@
-package sh.slb.breakingthing;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(BreakingThing.MODID)
-public class BreakingThing {
-   public static final String MODID = "breakingthing";
-}
