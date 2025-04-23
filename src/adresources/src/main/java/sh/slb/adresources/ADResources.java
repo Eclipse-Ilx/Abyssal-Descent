a8ccef301d30350b301f6f4301070a7da49c0e44
slbsh <crafter.antek@gmail.com>
2025-04-23 20:28:37 +0200

    change: 10 new placeholder blocks for ma boy eclip

--- src/adresources/src/main/java/sh/slb/adresources/ADResources.java
+++ src/adresources/src/main/java/sh/slb/adresources/ADResources.java
@@ -25,15 +25,37 @@ public class ADResources {
    public ADResources() {
       IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+		// TODO: wtf clean this up
       register_block("bedrock1", 2);
       register_block("bedrock2", 3);
       register_block("bedrock3", 4);
       register_block("bedrock4", 5);
       register_block("bedrock5", 6);
 
+		register_non_semaphore_block("ore0");
+		register_non_semaphore_block("ore1");
+		register_non_semaphore_block("ore2");
+		register_non_semaphore_block("ore3");
+		register_non_semaphore_block("ore4");
+		register_non_semaphore_block("ore5");
+		register_non_semaphore_block("ore6");
+		register_non_semaphore_block("ore7");
+		register_non_semaphore_block("ore8");
+		register_non_semaphore_block("ore9");
+
       BLOCKS.register(modEventBus);
       ITEMS.register(modEventBus);
    }
+
+	// TODO: prob invert naming cause sanity
+	private static void register_non_semaphore_block(String name) {
+		var block = BLOCKS.register(name, () -> new Block(
+			BlockBehaviour.Properties.of()
+				.mapColor(MapColor.STONE)
+				// .strength(50F, 36000F) // FIXME: idk what tf u want @eclipse
+		));
+		ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+	}
 
    private static void register_block(String name, int level) {
       var block = BLOCKS.register(name, () -> new SemaphoreBlock(
