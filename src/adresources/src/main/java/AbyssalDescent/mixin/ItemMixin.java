e21c22c0fe22bc1e878f27f2db95120f3a1bf049
slbsh <crafter.antek@gmail.com>
2025-11-26 15:05:17 +0100

    feat: modularize harvest level mixin

--- src/adresources/src/main/java/AbyssalDescent/mixin/ItemMixin.java
+++ src/adresources/src/main/java/AbyssalDescent/mixin/ItemMixin.java
@@ -3,23 +3,21 @@ package AbyssalDescent.adresources.mixin;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TieredItem;
+import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.item.Tiers;
-import net.minecraft.world.item.Tier;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import net.minecraft.world.item.PickaxeItem;
+
+import java.util.Optional;
 
 @Mixin(ItemStack.class)
 public abstract class ItemMixin {
@@ -28,11 +26,44 @@ public abstract class ItemMixin {
 
 	@Inject(method = "isCorrectToolForDrops", at = @At("HEAD"), cancellable = true)
 	private void onIsCorrectToolForDrops(BlockState state, CallbackInfoReturnable<Boolean> ci) {
-		if (state.is(Blocks.COPPER_ORE) || state.is(Blocks.DEEPSLATE_COPPER_ORE)) {
-			if (this.getItem() instanceof TieredItem tiered && tiered.getTier().getLevel() >= Tiers.WOOD.getLevel()) {
+		var id    = BuiltInRegistries.BLOCK.getKey(state.getBlock()).toString();
+
+		map_id_level(id).ifPresent(level -> {
+			if (this.getItem() instanceof PickaxeItem t && t.getTier().getLevel() >= level) {
 				ci.setReturnValue(true);
 				ci.cancel();
 			}
-		}
+		});
+	}
+
+	// theoretically should also work with modded blocks
+	// if there is no need for modded ones a faster approach would be to use Blocks.BLOCK_NAME
+	private static Optional<Integer> map_id_level(String id) {
+		return switch (id) {
+			case 
+				"minecraft:copper_ore"
+				-> Optional.of(0);
+			case
+				"minecraft:deepslate",
+				"minecraft:cobbled_deepslate",
+				"minecraft:polished_deepslate",
+				"minecraft:deepslate_bricks",
+				"minecraft:cracked_deepslate_bricks",
+				"minecraft:deepslate_tiles",
+				"minecraft:cracked_deepslate_tiles",
+				"minecraft:chiseled_deepslate",
+				"minecraft:deepslate_coal_ore",
+				"minecraft:deepslate_iron_ore",
+				"minecraft:deepslate_copper_ore",
+				"minecraft:deepslate_redstone_ore"
+				-> Optional.of(1);
+			case
+				"minecraft:deepslate_gold_ore",
+				"minecraft:deepslate_diamond_ore",
+				"minecraft:deepslate_lapis_ore",
+				"minecraft:deepslate_emerald_ore"
+				-> Optional.of(2);
+			default -> Optional.empty();
+		};
 	}
 }
